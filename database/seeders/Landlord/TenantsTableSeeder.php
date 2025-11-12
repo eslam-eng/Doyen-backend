a73ee77fdf7ad899135c6a45aa1589187984fa66
eslam <elbadry693@gmail.com>
2025-11-12 16:07:36 +0200

    create tenant and seed data

--- database/seeders/Landlord/TenantsTableSeeder.php
+++ database/seeders/Landlord/TenantsTableSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders\Landlord;
 
 use App\Models\Landlord\Tenant;
-use App\Models\Landlord\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class TenantsTableSeeder extends Seeder
 {
@@ -21,15 +19,5 @@ class TenantsTableSeeder extends Seeder
             'tenancy_db_name' => 'tenant_test', // optional, let package generate name
         ]);
         $tenant->domains()->create(['domain' => $tenant->slug]);
-
-        if (!$this->checkIfDatabaseExists($tenant->tenancy_db_name)){
-            event(new \Stancl\Tenancy\Events\TenantCreated($tenant));
-        }
-    }
-
-    private function checkIfDatabaseExists($databaseName)
-    {
-        $result = DB::select("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?", [$databaseName]);
-        return count($result) > 0;
     }
 }
